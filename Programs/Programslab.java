fa2675443b0b558f64d3cffebe6829a85f23b544
balram108 <balram108@gmail.com>
2022-04-19 13:26:36 -0400

    new commit

---
+++ Programs/Programslab.java
@@ -0,0 +1,41 @@
+class Primitiveslab {
+            
+	public static void main (String args[]){
+
+
+		boolean boo = true;
+
+		System.out.println(boo);
+
+		byte maxByte = 123;
+
+		System.out.println(maxByte);
+
+		short minShort = 12345;
+
+		System.out.println(minShort);
+
+		int maxInt = 659852;
+
+		System.out.println(maxInt);
+
+		long javaFlex = 3665;
+
+		System.out.println(javaFlex);
+
+		float pie = 236.2F;
+
+		System.out.println(pie);
+
+		
+
+
+
+
+
+	}
+
+
+
+
+}
