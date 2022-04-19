05216d901e5a8644e983c93a3d4e84958fd47efc
balram108 <balram108@gmail.com>
2022-04-19 02:04:11 -0400

    first commit

---
+++ Programs/Primitives.java
@@ -0,0 +1,44 @@
+class Primitives {
+         
+          public static void main(String args[]){
+                           
+		  //Lets look at some  primitives
+		  //boolean, a 1 or 0
+		  //datatype varName assignoperator valueof var
+                 boolean ourFirstBool = true;
+		 System.out.println(ourFirstBool);
+
+
+		 byte littleByte = 120;
+
+		 System.out.println(littleByte);
+
+		 short littleBitBigger = 30_000;
+
+		 short widening = littleBitBigger;
+
+		 System.out.println(widening);			
+
+		 int thirtyTwoBits = 12345678;
+
+		 short narrowing = (short) thirtyTwoBits;
+
+		 System.out.println(narrowing);
+
+		 long sixtyFourBits = 1234567890l;
+	
+		 float iamFloating = 2.34145f;
+
+                 double iamDoubleFloat = 56.232456436;
+
+		 char payRespect = 'F';
+
+		 System.out.println(payRespect);
+
+
+
+	  }
+	  
+
+}
+
